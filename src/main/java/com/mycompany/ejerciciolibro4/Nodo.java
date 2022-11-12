6de4a392ee56cadf2530ef6f07d225b159fbbbd8
Usuario <Usuario@LAPTOP-2RHH1TQ1>
2022-11-11 23:16:13 -0500

    fggfgfggfgf

---
+++ src/main/java/com/mycompany/ejerciciolibro4/Nodo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejerciciolibro4;
+
+/**
+ *
+ * @author Usuario
+ */
+class Nodo
+{
+String dato;
+Nodo enlace;
+public Nodo (String entrada) {; }
+}
